2032492911a4825a3c9c53c9b668386fb7d20eb2
tttela <ttteratttera@gmail.com>
2025-04-29 23:59:19 +0900

    expo動くけどスタイル当たらない

--- App.jsx
+++ App.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
+import { NativeWindStyleSheet } from 'nativewind';
+
+// NativeWindのスタイルシートを最適化（ネイティブ出力設定）
+NativeWindStyleSheet.setOutput({
+  default: 'native',
+});
 
 /**
  * RichmanManage アプリのメインコンポーネント
--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,22 @@
-module.exports = {
-  presets: ['babel-preset-expo'],
-  plugins: ['react-native-reanimated/plugin'], // ←これを必ず末尾に追加
+module.exports = function(api) {
+  api.cache(true);
+  
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@screens': './src/screens',
+            '@components': './src/components',
+            '@utils': './src/utils'
+          }
+        }
+      ],
+      'nativewind/babel',
+      'react-native-reanimated/plugin'
+    ]
+  };
 };
